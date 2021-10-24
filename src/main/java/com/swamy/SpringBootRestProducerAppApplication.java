7f3c8263c7649c42ab0ae5a9c43c7c3fa454725f
swamymasna <swamymasna55@gmail.com>
2021-10-24 19:58:13 +0530

    SBRP01: Committed Producer App

---
+++ src/main/java/com/swamy/SpringBootRestProducerAppApplication.java
@@ -0,0 +1,13 @@
+package com.swamy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProducerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProducerAppApplication.class, args);
+	}
+
+}
